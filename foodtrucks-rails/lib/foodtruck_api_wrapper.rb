b0c83d3230a8a9ce55d815b957983a348c54e63b
Alena Messmer <alena.messmer@nordstrom.com>
2018-08-13 22:17:10 -0700

    cache the api response

--- foodtrucks-rails/lib/foodtruck_api_wrapper.rb
+++ foodtrucks-rails/lib/foodtruck_api_wrapper.rb
@@ -2,11 +2,21 @@ require 'httparty'
 
 # queries the SF FoodTruck API and gets results
 # API link: https://dev.socrata.com/foundry/data.sfgov.org/bbb8-hzi6
-class FoodTruck
+class FoodtruckApiWrapper
 
   BASE_URL = 'http://data.sfgov.org/resource/bbb8-hzi6.json?'
 
   def initialize
+    @response = Rails.cache.fetch("foodtrucks", expires_in: 5.minutes) do
+      puts "loading response"
+      load_url
+    end
+
+    # set initial position to use in next_page
+    @position = 0
+  end
+
+  def load_url
     # set the timezone for this process to SF's timezone, so that time.now
     # returns SF local time and we get food trucks that are open now
     # even if that program is run in a different time zone
@@ -26,21 +36,26 @@ class FoodTruck
     # by their name. Because we are making changes to the names, this is no longer useful
     url = BASE_URL + "$where=dayorder=#{day} AND start24 <= '#{hour}' AND end24 > '#{hour}'&$order=applicant"
 
-    @response = HTTParty.get(url, follow_redirects: true)
+    response = HTTParty.get(url, follow_redirects: true)
 
     # httparty handles redirects, 400 and 500 are treated as an error
-    if @response.code != 200
+    if response.code != 200
       raise "error loading food truck data"
     end
 
     # modify the name to be more readable
-    trim_name(@response)
+    trim_name(response)
 
     # sorts truck names alphabetically, because we made changes to the name
-    @response.sort_by! { |truck| truck['applicant'] }
+    response.sort_by! { |truck| truck['applicant'] }
 
-    # set initial position to use in next_page
-    @position = 0
+    trucks = []
+
+    response.each do |item|
+      trucks << { "applicant" => item['applicant'], "location" => item['location']}
+    end
+
+    return trucks
   end
 
   # returns 10 results per page, if there are no results returns nil
